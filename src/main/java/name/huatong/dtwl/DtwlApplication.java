9d9ec9442b864b7bf25b4fd3a816130caa71beb7
mystic_hua <ht8309587>
2018-06-06 15:25:32 +0800

    first commit

---
+++ src/main/java/name/huatong/dtwl/DtwlApplication.java
@@ -0,0 +1,12 @@
+package name.huatong.dtwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtwlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtwlApplication.class, args);
+    }
+}
