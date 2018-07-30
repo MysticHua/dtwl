4860da80b083c2d59b93ef44f611d2e23bc8dea6
mystic_hua <ht8309587>
2018-07-30 16:23:59 +0800

    提交

--- src/main/java/name/huatong/dtwl/config/XxlJobConfig.java
+++ src/main/java/name/huatong/dtwl/config/XxlJobConfig.java
@@ -6,14 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * xxl-job config
  *
  * @author xuxueli 2017-04-28
  */
-@Configuration
+//@Configuration
 @ComponentScan(basePackages = "name.huatong.dtwl.quartz")
 public class XxlJobConfig {
     private Logger logger = LoggerFactory.getLogger(XxlJobConfig.class);
